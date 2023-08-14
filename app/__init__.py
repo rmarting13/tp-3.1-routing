a31b32cc8286329146ba4c29d0a4fa5add7e2986
Martin Gonzalez <rmarting13@gmail.com>
2023-08-14 11:45:42 -0300

    ejercicio1

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,8 @@ def init_app():
     app = Flask(__name__, static_folder = Config.STATIC_FOLDER, template_folder = Config.TEMPLATE_FOLDER)
     app.config.from_object(Config)
 
+    @app.route('/')
+    def hello_world():
+        return 'Bienvenidx!'
+
     return app
